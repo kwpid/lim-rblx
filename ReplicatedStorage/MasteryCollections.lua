25ca4a9615cf99926d4d6dbd76067c8afc9619fb
kwpid <143108314+kwpid@users.noreply.github.com>
2025-11-08 04:41:13 +0000

    asd

--- ReplicatedStorage/MasteryCollections.lua
+++ ReplicatedStorage/MasteryCollections.lua
@@ -2,27 +2,122 @@ local MasteryCollections = {}
 
 MasteryCollections.Collections = {
         {
-                Name = "Valkyrie Series",
-                ImageId = "rbxassetid://1234567890",
-                BadgeId = 0,
-                Items = {
-                        88275556285191,
-                }
-        },
-        {
-                Name = "Sparkle Time Collection",
-                ImageId = "rbxassetid://1234567891",
-                BadgeId = 0,
-                Items = {
-                }
-        },
-        {
                 Name = "Dominus Collection",
-                ImageId = "rbxassetid://1234567892",
-                BadgeId = 0,
+                ImageId = "rbxassetid://13109293573",
+                BadgeId = 2085155266275801,
                 Items = {
+                        21070012,
+                        48545806,
+                        162067148,
+                        31101391,
+                        138932314,
+                        250395631,
+                        335080779,
+                        64444871,
+                        96103379,
+                        527365852,
+                        4255053867
                 }
         },
+        {
+                Name = "Federation Collection",
+                ImageId = "rbxassetid://2527934631",
+                BadgeId = 2085155266275801,
+                Items = {
+                        128158708,
+                        88885069,
+                        293316452,
+                        207207782,
+                        1180419124,
+                        188003914,
+                        146134358,
+                        878889105,
+                        1340199684,
+                        4390900302,
+                        439984665,
+                        1180419690,
+                        4390896732
+                }
+        },
+        {
+                Name = "Domino Crown Collection",
+                ImageId = "rbxassetid://421829364",
+                BadgeId = 2085155266275801,
+                Items = {
+                        1031429,
+                        42211680,
+                        68258723,
+                        4390890198,
+                        1560601706,
+                        2606192523
+                }
+        },
+        {
+                Name = "Sparkle Time Fedora Collection",
+                ImageId = "rbxassetid://3145617426",
+                BadgeId = 2085155266275801,
+                Items = {
+                        72082328,
+                        259423244,
+                        119916949,
+                        63043890,
+                        334663683,
+                        100929604,
+                        147180077,
+                        493476042,
+                        215751161,
+                        1016143686,
+                        1285307
+                }
+        },
+        {
+                Name = "Valkyrie Collection",
+                ImageId = "rbxassetid://623062007",
+                BadgeId = 2085155266275801,
+                Items = {
+                        124730194,
+                        1180433861,
+                        2830437685,
+                        1365767,
+                        1402432199
+                }
+        },
+        {
+                Name = "Horn Collection",
+                ImageId = "rbxassetid://100630143590904",
+                BadgeId = 2085155266275801,
+                Items = {
+                        215718515,
+                        74891470,
+                        1744060292,
+                }
+        },
+        {
+                Name = "Immortal Collection",
+                ImageId = "rbxassetid://891846291",
+                BadgeId = 2085155266275801,
+                Items = {
+                        416828455,
+                        638089422,
+                        689391156,
+                        493486164,
+                        1678356850,
+                        2222720521,
+                        2041982658,
+                        2830451846,
+                        1425150624,
+                        568920951,
+                        4390848511,
+                        2566015124,
+                        2565909803,
+                        891980433,
+                        1402447683,
+                        835066252,
+                        1076571965,
+                        2606191948,
+                        2470750640
+                }
+        }
 }
 
 return MasteryCollections
